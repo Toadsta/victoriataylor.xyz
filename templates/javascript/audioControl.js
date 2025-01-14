1aa1a94aaf6d28ce9092aca8577e3b8f0613c427
Victoria <taylor-tori@proton.me>
2025-01-14 23:58:32 +0000

    Added/Modified - Added Windows 3.1 logo (temporary whilst I make my own version - Changed music to an eight bit rendition of luma from mario galaxy - Refactored JS code and CSS into their own template folder Removed - Legacy burger-menu.js from old website

---
+++ templates/javascript/audioControl.js
@@ -0,0 +1,20 @@
+// Get the audio and button elements
+const audio = document.getElementById('audio');
+const playPauseButton = document.getElementById('playPauseButton');
+
+// Set audio volume
+audio.volume = 0.2;
+
+// Function to toggle audio play/pause
+function toggleAudio() {
+    if (audio.paused) {
+        audio.play();
+        playPauseButton.querySelector('img').src = 'images/audioOn.png';  // Change image to "pause"
+    } else {
+        audio.pause();
+        playPauseButton.querySelector('img').src = 'images/audioOff.png';  // Change image to "play"
+    }
+}
+
+// Add an event listener to the button to toggle play/pause
+playPauseButton.addEventListener('click', toggleAudio);
